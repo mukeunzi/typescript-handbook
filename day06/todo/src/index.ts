b4ef223ee148b1e25e7427b2e18f6dbd8aded465
mukeunzi <eunji057@gmail.com>
2021-01-02 21:01:20 +0900

    Fix : 구체적인 타입 정의로 오류 해결
    - 인터페이스를 사용하지 않고 `todoItems`의 세부 타입을 지정함

--- day06/todo/src/index.ts
+++ day06/todo/src/index.ts
@@ -1,7 +1,7 @@
-let todoItems: object[]; // 배열이고, 인덱스의 값들이 객체 형태임
+let todoItems: { id: number, title: string, done: boolean }[]; // 배열이고, 인덱스의 값들이 객체 형태임
 
 // api
-function fetchTodoItems(): object[] {
+function fetchTodoItems(): { id: number, title: string, done: boolean }[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -16,7 +16,7 @@ function fetchTodos(): object[] {
   return todos;
 }
 
-function addTodo(todo: object): void {
+function addTodo(todo: { id: number, title: string, done: boolean }): void {
   todoItems.push(todo);
 }
 
@@ -24,8 +24,11 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: object): void {
-  todo.done = true; // item.done에서 오류 발생
+function completeTodo(
+  index: number, 
+  todo: { id: number, title: string, done: boolean }
+): void {
+  todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
@@ -35,12 +38,14 @@ function logFirstTodo(): object {
 }
 
 function showCompleted(): object[] {
-  return todoItems.filter((item: object) => item.done); // item.done에서 오류 발생
+  return todoItems.filter((item: { id: number, title: string, done: boolean }) => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
 function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  addTodo({ id: 4, title: '네번째', done: false });
+  addTodo({ id: 5, title: '다섯번째', done: false });
 }
 
 // NOTE: 유틸 함수
