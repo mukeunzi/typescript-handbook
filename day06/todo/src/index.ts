9fe23c9fc7191c62a660265f863d9ad55f641410
mukeunzi <eunji057@gmail.com>
2021-01-02 20:26:47 +0900

    Update : 모든 코드에 타입을 지정함
    - `noImplicitAny`값을 `true`로 변경하고, ts코드의 모든 변수와 함수에 타입을 지정함

--- day06/todo/src/index.ts
+++ day06/todo/src/index.ts
@@ -1,7 +1,7 @@
-let todoItems;
+let todoItems: object[]; // 배열이고, 인덱스의 값들이 객체 형태임
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): object[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,40 +11,40 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: object): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
-  todo.done = true;
+function completeTodo(index: number, todo: object): void {
+  todo.done = true; // item.done에서 오류 발생
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
-  return todoItems.filter(item => item.done);
+function showCompleted(): object[] {
+  return todoItems.filter((item: object) => item.done); // item.done에서 오류 발생
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
